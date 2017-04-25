8e13d600b824904219ed853b82a2e5e5faf5b381
Caesar0m0d <Caesar0m0d@gmail.com>
2017-04-25 18:10:38 +0900

    327A.c

---
+++ 327A.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+	int main(){
+
+int n,l,a[103];
+scanf("%d",&n);
+
+		for(l=0;l<n;l++){
+		if(l == (n-1)){scanf("%d",&a[l]);break;}
+scanf("%d ",&a[l]);
+				}
+
+
+return 0;
+		  }
